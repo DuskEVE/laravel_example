21cf989597e85ee2fec2a8bb0875e38e9e625512
DuskEVE <x987000@gmail.com>
2024-02-22 13:16:22 +0800

    first laravel practice project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container" style="text-align: center;">
+        <div><a href="./f1">F1</a></div>
+        <div><a href="./f2">F2</a></div>
+        <div><a href="./f3">F3</a></div>
+    </div>
+</body>
+</html>

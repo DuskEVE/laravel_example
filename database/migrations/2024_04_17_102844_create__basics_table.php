7c60adfb195530f545ea415cd19dfed543380cdb
DuskEVE <x987000@gmail.com>
2024-04-22 14:45:47 +0800

    adjust database config and run migration

--- database/migrations/2024_04_17_102844_create__basics_table.php
+++ database/migrations/2024_04_17_102844_create__basics_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('_basics', function (Blueprint $table) {
             $table->id();
+            $table->integer('NO');
+            $table->string('name');
+            $table->integer('amount');
             $table->timestamps();
         });
     }

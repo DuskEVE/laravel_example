1a356ad2fdcf803731e0792a34d03d0c14bc86b7
DuskEVE <x987000@gmail.com>
2024-08-25 07:59:31 +0800

    test how to use asset() to link css file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel example home</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-secondary">
+    <div class="top">
+        <div class="top-img"></div>
+    </div>
+
     <x-navbar>
         home page
     </x-navbar>
 
-    <div class="container-fluid">
+    <div class="content">
+
 
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,3 @@ Route::get('/', function () {
     return view('index');
     // return view('welcome');
 });
-Route::get('/one', function () {
-    return view('index');
-    // return view('welcome');
-});
-Route::get('/two', function () {
-    return view('index');
-    // return view('welcome');
-});
